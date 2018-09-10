6cadbdeb387d3e5ebd3ba6fd8ca98c8aab81ef62
clm <601974060@qq.com>
2018-09-10 22:44:51 +0800

    123

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
+
+import Login from '@/views/ '
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+    routes: [
+        { name: 'login', path: '/login', component: 'Login' }
+    ]
+});
